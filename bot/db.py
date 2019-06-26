24ad020a2b090b7263db126014eaa03d40b6d3a3
Stephen Edwards <sventi555@gmail.com>
2019-06-25 23:57:35 -0400

    check_for_perm db method

--- bot/db.py
+++ bot/db.py
@@ -11,7 +11,7 @@ def create_permset(db, role, name: str, permissions: list):
     if db.exists(key):
         return
 
-    db.sadd(key, *[perm.value for perm in permissions])
+    db.sadd(key, *[str(perm.value).encode('utf-8') for perm in permissions])
 
 
 def create_role(db, role, user):
@@ -25,4 +25,27 @@ def create_role(db, role, user):
         return
 
     create_permset(db, role, "administrators", Permission.all())
-    db.set(f"{key}:member:{user.id}", "administrators")
+    db.set(f"{key}:member:{user.id}", "administrators".encode('utf-8'))
+
+
+def check_for_perm(db, role, user, permission):
+    """
+    Check a user for a specific permission.
+    """
+
+    # get the user's permset for that role
+    user_key = f"guild:{role.guild.id}:role:{role.id}:member:{user.id}"
+    permset = db.get(user_key).decode('utf-8')
+
+    # if the user does not have a permset for the specified role, return false
+    if permset == 'None':
+        return False
+
+    # get all the permissions associated with that permset
+    permset_key = f"guild:{role.guild.id}:role:{role.id}:permset:{permset}"
+    permissions = list(map(
+        lambda p: int(p.decode('utf-8')),
+        db.smembers(permset_key)))
+
+    # return whether or not the specified permission is in the permset
+    return permission.value in permissions
