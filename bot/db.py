0962c2b77fe9d66eec1f5ebaf6979c9cbf17fb13
Mateja Lasan <mateja@lasan.ca>
2019-06-26 11:26:14 -0400

    Minor tweaks - Redis class automatically encodes data into bytes before storing. Encoding it by hand is unecessary. - .encode() and .decode() default to utf-8 - map() performs worse than list comprehensions when using a lambda.

--- bot/db.py
+++ bot/db.py
@@ -11,7 +11,7 @@ def create_permset(db, role, name: str, permissions: list):
     if db.exists(key):
         return
 
-    db.sadd(key, *[str(perm.value).encode('utf-8') for perm in permissions])
+    db.sadd(key, *[perm.value for perm in permissions])
 
 
 def create_role(db, role, user):
@@ -25,7 +25,7 @@ def create_role(db, role, user):
         return
 
     create_permset(db, role, "administrators", Permission.all())
-    db.set(f"{key}:member:{user.id}", "administrators".encode('utf-8'))
+    db.set(f"{key}:member:{user.id}", "administrators")
 
 
 def check_for_perm(db, role, user, permission):
@@ -35,17 +35,15 @@ def check_for_perm(db, role, user, permission):
 
     # get the user's permset for that role
     user_key = f"guild:{role.guild.id}:role:{role.id}:member:{user.id}"
-    permset = db.get(user_key).decode('utf-8')
+    permset = db.get(user_key).decode()
 
     # if the user does not have a permset for the specified role, return false
-    if permset == 'None':
+    if not permset:
         return False
 
     # get all the permissions associated with that permset
     permset_key = f"guild:{role.guild.id}:role:{role.id}:permset:{permset}"
-    permissions = list(map(
-        lambda p: int(p.decode('utf-8')),
-        db.smembers(permset_key)))
+    permissions = [int(p.decode()) for p in db.smembers(permset_key)]
 
     # return whether or not the specified permission is in the permset
     return permission.value in permissions
