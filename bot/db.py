ab41bf6eefc55871505925ba5085cc2dcbaf66fe
Mateja Lasan <mateja@lasan.ca>
2019-06-27 23:00:11 -0400

    ORMified db.py

--- bot/db.py
+++ bot/db.py
@@ -1,168 +1,358 @@
+import re
+
+from redis import Redis
+
 from bot.utils import Permission
 
 
-def get_permset(db, role, name: str) -> list or None:
+class db_connection:
+    def __enter__(self):
+        db = Redis()
+        return db
+
+    def __exit__(self, type, value, traceback):
+        pass
+
+
+class CreationError(Exception):
+    pass
+
+
+class DataSet:
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+
+class Permset(DataSet):
     """
-    Get all the permissions for a permission set.
-    """
-
-    key = f"guild:{role.guild.id}:role:{role.id}:permset:{name}"
-
-    permints = db.smembers(key)
-
-    if permints:
-        permissions = [Permission.from_value(p.decode()) for p in permints]
-        return permissions
-    return None
-
-
-def get_giveable_permset(db, role, user) -> list:
-    """
-    Get a list of giveable permissions for a user.
-    """
-
-    user_key = f"guild:{role.guild.id}:role:{role.id}:member:{user.id}"
-    permset = db.get(user_key)
-
-    if not permset:
-        return False
-
-    permissions = get_permset(db, role, permset.decode())
-
-    if Permission.CREATE_PERMSETS in permissions:
-        permissions.pop(permissions.index(Permission.CREATE_PERMSETS))
-
-    return permissions
-
-
-def create_permset(db, role, name: str, permissions: list) -> (bool, str):
-    """
-    Create a permission set entry in the database.
+    Object representing a permset database entry.
     """
 
-    key = f"guild:{role.guild.id}:role:{role.id}:permset:{name}"
+    @property
+    def giveable(self):
+        """
+        Return a list of permissions that can be granted to new Permsets
+        created by a member of this Permset.
+        """
 
-    if db.exists(key):
-        return False, "Permset entry already exists"
+        permissions = self.permissions
 
-    db.sadd(key, *[perm().value for perm in permissions])
-    return True, "Created permsets"
+        try:
+            permissions.remove(Permission.CREATE_PERMSETS)
+            return permissions
+        except ValueError:
+            return None
+
+    @property
+    def role(self):
+        """
+        Return a role this permset is related to.
+        """
+
+        match = re.search(r"(*):permset:", self.key_prefix)
+        return Role.get_raw(match.group(1))
+
+    @property
+    def _member_keys(self) -> list:
+        """
+        Return all members of this permset.
+        """
+
+        match = re.search(r"(*):permset:", self.key_prefix)
+        match = f"{match.group(1)}:member:*"
+
+        with db_connection() as db:
+            def check(result):
+                return True if db.get(result) == self.name else False
+
+            results = filter(check, db.scan_iter(match=match))
+
+        return [r.decode() for r in results]
+
+    @staticmethod
+    def exists(
+            role: object,
+            name: str) -> bool:
+        """
+        Check if a permset exists.
+        """
+
+        key = f"{role.key}:permset:{name}"
+
+        with db_connection() as db:
+            return db.exists(key)
+
+    @staticmethod
+    def get(
+            role: object,
+            name: str) -> object or None:
+        """
+        Get a Permset object by role and name.
+        """
+
+        return get_raw(f"guild:{role.guild.id}:role:{role.id}:permset:{name}")
+
+    @staticmethod
+    def get_raw(
+            key: str) -> list:
+        """
+        Get a Permset object by database key.
+        """
+
+        with db_connection() as db:
+            permints = db.smembers(key)
+
+        if permints:
+            permissions = [Permission.from_value(p.decode()) for p in permints]
+            match = re.search(r"(guild:\d+:role:\d+:permset:)(.+$)", key)
+            key_prefix = match.group(1)
+            name = match.group(2)
+            return Permset(
+                name=name,
+                key_prefix=key_prefix,
+                permissions=permissions)
+        return None
+
+    @staticmethod
+    def get_all(
+            role: object) -> list:
+        """
+        Get all Permsets for a role.
+        """
+
+        match = f"{role.key}:permset:*"
+
+        with db_connection() as db:
+            results = db.scan_iter(match=match)
+
+        return [Permset.get_raw(key.decode()) for key in results]
+
+    @staticmethod
+    def for_user(
+            role,
+            user):
+        """
+        Get the Permset object for a user.
+        """
+
+        key_prefix = f"guild:{role.guild.id}:role:{role.id}:"
+
+        with db_connection() as db:
+            name = db.get(
+                f"{key_prefix}member:{user.id}")
+
+        return Permset.get_raw(f"{key_prefix}permset:{name}")
+
+    @staticmethod
+    def create(
+            role: object,
+            name: str,
+            permissions: list) -> object:
+        """
+        Create an entry for a permission set in the database.
+        Return an object representing that entry.
+        """
+
+        permints = *[perm().value for perm in permissions]
+        key_prefix = f"guild:{role.guild.id}:role:{role.id}:permset:"
+        key = f"{key_prefix}{name}"
+
+        with db_connection() as db:
+            if db.exists(key):
+                raise CreationError(
+                    "Permission set entry already exists.")
+            db.sadd(key, permints)
+
+        return Permset(
+            name=name,
+            key_prefix=key_prefix,
+            permissions=permissions)
+
+    def update(
+            self,
+            name: str = None,
+            permissions: list = None):
+        """
+        Update a Permset in place, as well as its database entry.
+        """
+
+        if name:
+            with db_connection() as db:
+                permints = db.smembers(f"{self.key_prefix}{self.name}")
+                db.sadd(f"{self.key_prefix}{name}", *permints)
+                db.delete(f"{self.key_prefix}{self.name}")
+            self.name = name
+
+        if permissions:
+            permints = *[perm().value for perm in permissions]
+            with db_connection() as db:
+                db.delete(f"{self.key_prefix}{self.name}")
+                db.sadd(f"{self.key_prefix}{self.name}", *permints)
+            self.permissions = permissions
+
+    def delete(
+            self):
+        """
+        Delete a Permset and its database entry.
+        """
+
+        with db_connection() as db:
+            db.delete(f"{self.key_prefix}{self.name}", *self._member_keys)
 
 
-def edit_permset(db, role, name: str, permissions: list) -> (bool, str):
+class Role(DataSet):
     """
-    Modify a permission set entry in the database.
+    Object representing a role database entry.
     """
 
-    key = f"guild:{role.guild.id}:role:{role.id}:permset:{name}"
+    @property
+    def members(self) -> list:
+        """
+        All members in this role.
+        """
 
-    if not db.exists(key):
-        return False, "Permset does not exist"
+        match = f"{self.key}:member:*"
 
-    db.delete(key)
-    db.sadd(key, *[perm().value for perm in permissions])
-    return True, "Permset configured"
+        with db_connection() as db:
+            results = db.scan_iter(match=match)
 
+        members = []
+        for result in results:
+            match = re.search(r"*:member:(.+$)", result.decode())
+            user_id = int(match.group(1))
+            members.append(user_id)
 
-def delete_permset(db, role, name: str):
-    """
-    Delete a permission set entry from the database.
-    """
+        return members
 
-    delete_all_members_under(db, role, name)
-    key = f"guild:{role.guild.id}:role:{role.id}:permset:{name}"
-    db.delete(key)
+    @property
+    def permsets(self) -> list:
+        """
+        All permsets for this role.
+        """
 
+        return Permset.get_all(self)
 
-def delete_all_permsets(db, role):
-    """
-    Delete all permsets related to a role.
-    """
+    def add_member(
+            self,
+            user: object,
+            permset: str) -> None:
+        """
+        Add a user to this role under a permset by its name.
+        """
 
-    pattern = f"guild:{role.guild.id}:role:{role.id}:permset:*"
+        if Permset.exists(self, permset):
+            user_key = f"{self.key}:member:{user.id}"
 
-    results = db.scan_iter(match=pattern)
+            with db_connection() as db:
+                if db.exists(user_key):
+                    raise CreationError()
+                db.set(user_key, permset)
 
-    for result in results:
-        db.delete(result)
+    def update_member(
+            self,
+            user: object,
+            permset: str) -> None:
+        """
+        Change a members permset.
+        """
 
+        if Permset.exists(self, permset):
+            user_key = f"{self.key}:member:{user.id}"
 
-def create_role(db, role, user):
-    """
-    Create a role entry in the database.
-    """
+            with db_connection() as db:
+                if db.exists(user_key):
+                    db.set(user_key, permset)
 
-    key = f"guild:{role.guild.id}:role:{role.id}"
+    def check_member_for_perm(
+            self,
+            user: object,
+            permission: object):
+        """
+        Check a user for a specific permission.
+        """
 
-    if db.exists(key):
-        return
+        user_key = f"{self.key}:member:{user.id}"
 
-    create_permset(db, role, "administrators", Permission.all())
-    db.set(f"{key}:member:{user.id}", "administrators")
+        with db_connection() as db:
+            permset_name = db.get(user_key).decode()
+        permset = Permset.get(self, permset_name)
 
+        return permission in permset.permissions
 
-def create_member(db, role, user, permset):
-    """
-    Create a member entry for a permset.
-    """
+    def remove_member(
+            self,
+            user: object) -> None:
+        """
+        Remove a member from a role.
+        """
 
-    permset_key = f"guild:{role.guild.id}:role:{role.id}:permset:{permset}"
-    if db.exists(permset_key):
-        key = f"guild:{role.guild.id}:role:{role.id}:member:{user.id}"
-        db.set(key, permset)
+        user_key = f"{self.key}:member:{user.id}"
 
+        with db_connection() as db:
+            db.delete(user_key)
 
-def delete_member(db, role, user):
-    """
-    Delete a member entry from the database.
-    """
+    @staticmethod
+    def get_raw(
+            key: str) -> object:
+        """
+        Get a Role object by key.
+        """
 
-    key = f"guild:{role.guild.id}:role:{role.id}:member:{user.id}"
-    db.delete(key)
+        with db_connection() as db:
+            if not db.exists(key):
+                return None
 
+        return Role(
+            key=key)
 
-def delete_all_members(db, role):
-    """
-    Delete all members of a role.
-    """
+    @staticmethod
+    def get_for(
+            role: object) -> object:
+        """
+        Get a Role object for Discord role.
+        """
 
-    pattern = f"guild:{role.guild.id}:role:{role.id}:member:*"
+        return Role.get_raw(f"guild:{role.guild.id}:role:{role.id}")
 
-    results = db.scan_iter(match=pattern)
+    @staticmethod
+    def create(
+            role,
+            user) -> object:
+        """
+        Create an entry for a permission set in the database.
+        Return an object representing that entry.
+        """
 
-    for result in results:
-        db.delete(result)
+        key = f"guild:{role.guild.id}:role:{role.id}"
+        user_key = f"{key}:member:{user.id}"
 
+        with db_connection() as db:
+            if db.exists(key):
+                raise CreationError(
+                    "Role already exists.")
 
-def delete_all_members_under(db, role, permset):
-    """
-    Delete all members of a permset
-    """
+            admin = Permset.create(
+                role,
+                "administrators",
+                Permission.all())
+            db.set(user_key, admin.name)
 
-    pattern = f"guild:{role.guild.id}:role:{role.id}:member:*"
+        return Role(
+            key=key)
 
-    results = db.scan_iter(match=pattern)
+    def delete(
+            self) -> None:
+        """
+        Delete a role and its data.
+        """
 
-    for result in results:
-        if db.get(result).decode() == permset:
-            db.delete(result)
+        for permset in self.permsets:
+            permset.delete()
 
+        with db_connection() as db:
+            for member in self.members:
+                key = f"{self.key}:member:{member}"
+                db.delete(key)
 
-def check_for_perm(db, role, user, permission):
-    """
-    Check a user for a specific permission.
-    """
-
-    # get the user's permset for that role
-    user_key = f"guild:{role.guild.id}:role:{role.id}:member:{user.id}"
-    permset = db.get(user_key)
-
-    # if the user does not have a permset for the specified role, return false
-    if not permset:
-        return False
-
-    permset = permset.decode()
-
-    # get all the permissions associated with that permset
-    permset_key = f"guild:{role.guild.id}:role:{role.id}:permset:{permset}"
-    return db.sismember(permset_key, permission().value)
+            db.delete(self.key)
